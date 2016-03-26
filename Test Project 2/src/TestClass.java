f571812a7611444899f478a6bae6ef4075acc052
Tim Sebring <tim.sebring@gmail.com>
2016-03-25 17:28:27 -0700

    Another change testing

--- Test Project 2/src/TestClass.java
+++ Test Project 2/src/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
 //  projects and how they interact with my GitHub account
 	public static void main(String args[]) {
 		System.out.println("This is a test");
+		System.out.println("Further testing");
 	}
 }
